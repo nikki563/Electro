74b3dda806c9e90989c600afc9079c4aed58196c
nikki563 <117338375+nikki563@users.noreply.github.com>
2023-12-25 18:55:00 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-(function($) {
+(function(₹) {
 	"use strict"
 
 	// Mobile Nav toggle
-	$('.menu-toggle > a').on('click', function (e) {
+	₹('.menu-toggle > a').on('click', function (e) {
 		e.preventDefault();
-		$('#responsive-nav').toggleClass('active');
+		₹('#responsive-nav').toggleClass('active');
 	})
 
 	// Fix cart dropdown from closing
-	$('.cart-dropdown').on('click', function (e) {
+	₹('.cart-dropdown').on('click', function (e) {
 		e.stopPropagation();
 	});
 
 	/////////////////////////////////////////
 
 	// Products Slick
-	$('.products-slick').each(function() {
-		var $this = $(this),
-				$nav = $this.attr('data-nav');
+	₹('.products-slick').each(function() {
+		var ₹this = ₹(this),
+				$nav = ₹this.attr('data-nav');
 
-		$this.slick({
+		₹this.slick({
 			slidesToShow: 4,
 			slidesToScroll: 1,
 			autoplay: true,
@@ -27,7 +27,7 @@
 			speed: 300,
 			dots: false,
 			arrows: true,
-			appendArrows: $nav ? $nav : false,
+			appendArrows: ₹nav ? ₹nav : false,
 			responsive: [{
 	        breakpoint: 991,
 	        settings: {
@@ -47,24 +47,24 @@
 	});
 
 	// Products Widget Slick
-	$('.products-widget-slick').each(function() {
-		var $this = $(this),
-				$nav = $this.attr('data-nav');
+	₹('.products-widget-slick').each(function() {
+		var ₹this = ₹(this),
+				₹nav = ₹this.attr('data-nav');
 
-		$this.slick({
+		₹this.slick({
 			infinite: true,
 			autoplay: true,
 			speed: 300,
 			dots: false,
 			arrows: true,
-			appendArrows: $nav ? $nav : false,
+			appendArrows: ₹nav ? ₹nav : false,
 		});
 	});
 
 	/////////////////////////////////////////
 
 	// Product Main img Slick
-	$('#product-main-img').slick({
+	₹('#product-main-img').slick({
     infinite: true,
     speed: 300,
     dots: false,
@@ -97,31 +97,31 @@
 	// Product img zoom
 	var zoomMainProduct = document.getElementById('product-main-img');
 	if (zoomMainProduct) {
-		$('#product-main-img .product-preview').zoom();
+		₹('#product-main-img .product-preview').zoom();
 	}
 
 	/////////////////////////////////////////
 
 	// Input number
-	$('.input-number').each(function() {
-		var $this = $(this),
-		$input = $this.find('input[type="number"]'),
-		up = $this.find('.qty-up'),
-		down = $this.find('.qty-down');
+	₹('.input-number').each(function() {
+		var ₹this = ₹(this),
+		₹input = ₹this.find('input[type="number"]'),
+		up = ₹this.find('.qty-up'),
+		down = ₹this.find('.qty-down');
 
 		down.on('click', function () {
-			var value = parseInt($input.val()) - 1;
+			var value = parseInt(₹input.val()) - 1;
 			value = value < 1 ? 1 : value;
-			$input.val(value);
-			$input.change();
-			updatePriceSlider($this , value)
+			₹input.val(value);
+			₹input.change();
+			updatePriceSlider(₹this , value)
 		})
 
 		up.on('click', function () {
-			var value = parseInt($input.val()) + 1;
-			$input.val(value);
-			$input.change();
-			updatePriceSlider($this , value)
+			var value = parseInt(₹input.val()) + 1;
+			₹input.val(value);
+			₹input.change();
+			updatePriceSlider(₹this , value)
 		})
 	});
 
@@ -129,11 +129,11 @@
 			priceInputMin = document.getElementById('price-min');
 
 	priceInputMax.addEventListener('change', function(){
-		updatePriceSlider($(this).parent() , this.value)
+		updatePriceSlider(₹(this).parent() , this.value)
 	});
 
 	priceInputMin.addEventListener('change', function(){
-		updatePriceSlider($(this).parent() , this.value)
+		updatePriceSlider(₹(this).parent() , this.value)
 	});
 
 	function updatePriceSlider(elem , value) {
